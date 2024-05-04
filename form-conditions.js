fe1407e386a2202b878655d943da8a3decd60298
Carlos Torres <ctorresmoral@gmail.com>
2024-05-04 14:42:44 +0200

    add form validation

---
+++ form-conditions.js
@@ -0,0 +1,82 @@
+const formConditions = [
+  {
+    conditionName: "Full name should have at least one character",
+    invalidMessage: "Please enter your full name",
+    fieldName: "full-name",
+    condition: (fieldValue) => fieldValue.trim().length > 0,
+  },
+  {
+    conditionName:
+      "Full name should include name and surname separated by a space",
+    invalidMessage:
+      "Please enter your first name and your surname separated by a space",
+    fieldName: "full-name",
+    condition: (fieldValue) => fieldValue.includes(" "),
+  },
+  {
+    conditionName: "Email should have at least one character",
+    invalidMessage: "Please enter your email address",
+    fieldName: "email",
+    condition: (fieldValue) => fieldValue.trim().length > 0,
+  },
+  // The email validation conditions can be improved. For more
+  // details, see https://en.wikipedia.org/wiki/Email_address#Syntax
+  {
+    conditionName:
+      "Email should start with a set of letters, numbers, dots, or hyphens",
+    invalidMessage: "Please enter a valid email username",
+    fieldName: "email",
+    condition: (fieldValue) => {
+      const emailUsernameRegex = /^[\w-\.]+/
+      return emailUsernameRegex.test(fieldValue)
+    },
+  },
+  {
+    conditionName: "Email should include a @ symbol",
+    invalidMessage: "Please include a @ symbol",
+    fieldName: "email",
+    condition: (fieldValue) => fieldValue.includes("@"),
+  },
+  {
+    conditionName: "Email should end with a valid domain name",
+    invalidMessage: "Please enter a valid email domain",
+    fieldName: "email",
+    condition: (fieldValue) => {
+      const domainRegex = /([\w-]+\.)+[\w-]{2,4}$/
+      return domainRegex.test(fieldValue)
+    },
+  },
+  {
+    conditionName: "Phone number should have at least one character",
+    invalidMessage: "Please enter your phone number",
+    fieldName: "phone",
+    condition: (fieldValue) => fieldValue.trim().length > 0,
+  },
+  {
+    conditionName:
+      "Phone number should include only numbers, spaces, or hyphens. No letters or special characters allowed",
+    invalidMessage:
+      "Please include only numbers. You may also separate them with spaces or hyphens",
+    fieldName: "phone",
+    condition: (fieldValue) => {
+      const numericPhoneRegex = /^[0-9 -]+$/
+      return numericPhoneRegex.test(fieldValue)
+    },
+  },
+  {
+    conditionName: "Phone number should be a 9-or-10-digit number",
+    invalidMessage: "Please enter just 9 or 10 digits in total",
+    fieldName: "phone",
+    condition: (fieldValue) => {
+      const onlyDigits = fieldValue.replace(/\D/g, "")
+      console.log("onlyDigits", onlyDigits, "length", onlyDigits.length)
+      return onlyDigits.length > 8 && onlyDigits.length < 11
+    },
+  },
+  {
+    conditionName: "Message should have at least one character",
+    invalidMessage: "Please enter a message",
+    fieldName: "message",
+    condition: (fieldValue) => fieldValue.trim().length > 0,
+  },
+]
---
+++ form-validation.js
@@ -0,0 +1,168 @@
+const form = document.querySelector(".contact-us")
+// to do: handle error if some is not instanceof HTMLElement
+
+const CONSTANTS = {
+  LOCALSTORAGE_KEY: "contact-us-form-data",
+}
+
+const previousFormData = localStorage_getForm()
+if (previousFormData) {
+  handleSuccessUi(previousFormData)
+} else {
+  form.addEventListener("submit", handleSubmit)
+}
+
+function handleSubmit(submitEvent) {
+  submitEvent.preventDefault()
+
+  const formData = new FormData(form)
+  const isValid = validateForm(formData)
+  if (!isValid) return
+
+  formData.append("date", Date.now())
+  formData.append("time-spent", submitEvent.timeStamp)
+
+  // to do: send formData somewhere
+
+  localStorage_setForm(formData)
+  handleSuccessUi(formData)
+}
+
+function validateForm(formData) {
+  const validations = formConditions.map(
+    ({ fieldName, condition, invalidMessage }) => {
+      const fieldValue = formData.get(fieldName)
+      const meets = condition(fieldValue)
+      return { meets, fieldName, invalidMessage }
+    }
+  )
+
+  const areAllValid = validations.every((validation) => validation.meets)
+  if (areAllValid) {
+    return true
+  } else {
+    handleInvalidFieldsUi(validations)
+    return false
+  }
+}
+
+function handleInvalidFieldsUi(validationResults) {
+  const invalidHandledFields = []
+  validationResults.forEach((validation) => {
+    // If it's already been handled as invalid, skip it
+    if (invalidHandledFields.includes(validation.fieldName)) return
+
+    // Get errorMessageNode
+    const fieldNode = getFieldNode(validation.fieldName)
+    // to do: handle error if some is not instanceof HTMLElement
+    const labelNode = fieldNode.closest("label")
+    // to do: handle error if some is not instanceof HTMLElement
+    const errorMessageNode = labelNode.querySelector(".error-message-text")
+    // to do: handle error if some is not instanceof HTMLElement
+
+    // Perform UI changes
+    const [isValid, classMethod, errorMessage] = validation.meets
+      ? [true, "remove", ""]
+      : [false, "add", validation.invalidMessage]
+    fieldNode.classList[classMethod]("invalid")
+    errorMessageNode.textContent = errorMessage
+    if (invalidHandledFields.length < 1) {
+      fieldNode.focus()
+    }
+
+    // Record invalid field as handled
+    if (!isValid) {
+      invalidHandledFields.push(validation.fieldName)
+    }
+  })
+}
+
+function getFieldNode(fieldName) {
+  return form.querySelector(`[name=${fieldName}]`)
+}
+
+function handleSuccessUi(formData) {
+  const handleFieldMethods = {
+    date: getDate,
+    "time-spent": getAmountOfTime,
+  }
+
+  const formKeys = Array.from(formData.keys())
+  const messageDetailsHtml = formKeys
+    .map((fieldKey) => {
+      const fieldValue = formData.get(fieldKey)
+
+      const displayValue =
+        handleFieldMethods[fieldKey]?.(fieldValue) || fieldValue
+
+      return `<dt>${fieldKey}</dt><dd>${displayValue}</dd>`
+    })
+    .join("")
+
+  // to do: this shouldn't be in a form element, just do it outside and remove the form
+  form.innerHTML =
+    `<div class="contact-message-success">` +
+    /**/ `<h2>Thank you for contacting us!</h2>` +
+    /**/ `<p>Please expect a response within the next 24 business hours.</p>` +
+    /**/ `<p>You can review the message sent below:</p>` +
+    /**/ `<dl class="contact-message-success-details">${messageDetailsHtml}</dl>` +
+    /**/ `<p>If you need immediate assistance, please call us at <a href="tel:111 111 111">111 111 111</a>.</p>` +
+    /**/ `<div>` +
+    /**/ /**/ `<button class="new-form button-primary">Send another message</button>` +
+    /**/ `</div>` +
+    `</div>`
+  const newFormButton = form.querySelector(".new-form")
+  newFormButton.addEventListener("click", () => {
+    localStorage_deleteForm()
+    location.reload()
+  })
+}
+
+function getDate(rawTimestamp) {
+  const timestamp = Number(rawTimestamp)
+  if (isNaN(timestamp)) {
+    console.error("Invalid date", rawTimestamp)
+    return rawTimestamp
+  }
+
+  return new Date(timestamp).toLocaleDateString(navigator.language, {
+    hour: "numeric",
+    minute: "numeric",
+  })
+}
+
+function getAmountOfTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+  const getPluralSuffix = (value) => (value === 1 ? "" : "s")
+  return minutes < 1
+    ? `${seconds} second${getPluralSuffix(seconds)}`
+    : hours < 1
+    ? `${minutes} minute${getPluralSuffix(minutes)}`
+    : `${hours} hour${getPluralSuffix(hours)}`
+}
+
+function localStorage_setForm(formData) {
+  const formDataObject = Object.fromEntries(formData.entries())
+  const formDataString = JSON.stringify(formDataObject)
+  localStorage.setItem(CONSTANTS.LOCALSTORAGE_KEY, formDataString)
+}
+
+function localStorage_getForm() {
+  const formDataString = localStorage.getItem(CONSTANTS.LOCALSTORAGE_KEY)
+  if (!formDataString) return null
+
+  const formDataObject = JSON.parse(formDataString)
+
+  const formData = new FormData()
+  for (const key in formDataObject) {
+    formData.append(key, formDataObject[key])
+  }
+
+  return formData
+}
+
+function localStorage_deleteForm() {
+  localStorage.removeItem(CONSTANTS.LOCALSTORAGE_KEY)
+}
