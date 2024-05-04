10090f59f99cebb7d2263dc4c72dae7b620b293c
Carlos Torres <ctorresmoral@gmail.com>
2024-05-04 09:25:04 +0200

    refactor parts of router.js

--- router.js
+++ router.js
@@ -1,21 +1,23 @@
-getAndDisplayData()
+const PROJECT_ID_KEY = "id"
+const ENDPOINT =
+  "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
 
-function getAndDisplayData() {
-  const ENDPOINT =
-    "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
-
-  fetch(ENDPOINT)
-    .then((response) => response.json())
-    .then(displayData)
-    .catch((error) => {
-      // to do: handle this
-    })
-}
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then(displayData)
+  .catch((error) => {
+    // to do: handle this
+    console.error(error)
+  })
 
 function displayData(projectsData) {
   // to do: handle no projectsData...
+  // comprobar la reponse?
+  // validar que la response tiene la forma adecuada
 
-  const currentProjectId = new URLSearchParams(location.search).get("id")
+  const currentProjectId = new URLSearchParams(location.search).get(
+    PROJECT_ID_KEY
+  )
 
   const projectArea = document.querySelector("#project")
   if (projectArea instanceof HTMLElement) {
@@ -29,17 +31,19 @@ function displayData(projectsData) {
 }
 
 function displayProject(projectsData, currentProjectId, projectArea) {
-  // to do: handle no currentProjectId...
+  // to do: handle no currentProjectId... 404 scenario
+  // message: "no project ID"
 
   const projectData = projectsData.find(
     (project) => project.uuid === currentProjectId
   )
-  // to do: handle no projectData...
+  // to do: handle no projectData... 404 scenario
+  // message: "no project has ID X"
 
   fillNodesWithData(projectData, {
+    time: projectArea.querySelector(".project-details-time time"),
     title: projectArea.querySelector("h1"),
     summary: projectArea.querySelector(".project-details-summary"),
-    time: projectArea.querySelector(".project-details-time time"),
     mainImg: projectArea.querySelector(".image-main"),
     glowImg: projectArea.querySelector(".image-glow"),
     description: projectArea.querySelector(".project-description"),
@@ -53,8 +57,6 @@ function displayOtherProjects(
   currentProjectId,
   otherProjectsArea
 ) {
-  // to do: handle no currentProjectId...
-
   const otherProjectsData = getRandomProjects(projectsData, currentProjectId)
   const otherProjectsCards = otherProjectsArea.querySelectorAll(".card")
 
@@ -68,46 +70,48 @@ function displayOtherProjects(
 
     fillNodesWithData(projectData, {
       img: card.querySelector(".card-image img"),
+      link: card.querySelector(".card-body a"),
       title: card.querySelector(".card-body h3"),
       summary: card.querySelector(".card-body p"),
-      link: card.querySelector(".card-body a"),
     })
   }
 }
 
 function fillNodesWithData(projectData, nodes) {
+  const fillImgNode = (imgNode) => {
+    imgNode.src = projectData.image
+    imgNode.alt = `Image of project ${projectData.name}`
+  }
+
   if (nodes.title) {
     nodes.title.textContent = projectData.name
   }
   if (nodes.summary) {
     nodes.summary.textContent = projectData.description
   }
+  if (nodes.description) {
+    nodes.description.innerHTML = projectData.content
+  }
+  if (nodes.link) {
+    nodes.link.href = `/project.html?${PROJECT_ID_KEY}=${projectData.uuid}`
+  }
   if (nodes.time) {
     nodes.time.textContent = projectData.completed_on
     nodes.time.datetime = projectData.completed_on
   }
+  if (nodes.img) {
+    fillImgNode(nodes.img)
+  }
   if (nodes.mainImg) {
-    nodes.mainImg.src = projectData.image
-    nodes.mainImg.alt = `Image of project ${projectData.name}`
+    fillImgNode(nodes.mainImg)
   }
   if (nodes.glowImg) {
-    nodes.glowImg.src = projectData.image
-    nodes.glowImg.alt = `Image of project ${projectData.name}`
-  }
-  if (nodes.description) {
-    nodes.description.innerHTML = projectData.content
-  }
-  if (nodes.img) {
-    nodes.img.src = projectData.image
-    nodes.img.alt = `Image of project ${projectData.name}`
-  }
-  if (nodes.link) {
-    nodes.link.href = `/project.html?id=${projectData.uuid}`
+    fillImgNode(nodes.glowImg)
   }
 }
 
 function shuffle(array) {
-  // Fisher-Yates Sorting Algorithm
+  // Fisher-Yates sorting algorithm
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
@@ -116,9 +120,10 @@ function shuffle(array) {
 }
 
 function getRandomProjects(projectsData, currentProjectId) {
-  const isAnother = (project) => project.uuid !== currentProjectId
   const otherProjectsData = currentProjectId
-    ? projectsData.slice().filter(isAnother)
+    ? projectsData
+        .slice()
+        .filter((project) => project.uuid !== currentProjectId)
     : projectsData.slice()
   return shuffle(otherProjectsData)
 }
